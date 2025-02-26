87fb81d9e791c7626e8ef6af5c224c8dde56a548
Mohit Kumar <94331227+M0h1it@users.noreply.github.com>
2025-02-26 16:23:56 +0000

    datatype_summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,23 @@
+//primitive
+
+//7 types : String, Number,Boolean,null ,undefined, Symbol, BigInt
+
+//JavaScript is a dynamically typed language
+
+//Reference(non primitive)
+
+// array, Objects, Functions
+
+//const heros = ["shaktiman", "nagraj"];
+//array
+
+//objects
+// let myObj = {
+//     name : "hitesh",
+//     age : 22,
+// }
+
+//function as variable
+// const myFunction = function(){
+//     console.log("hello world");
+// }
