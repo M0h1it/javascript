962abfa69f185fa82e401e81e2abe25b12977e5d
Mohit Kumar <94331227+M0h1it@users.noreply.github.com>
2025-02-26 13:56:20 +0000

    updated changes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
-let score =33;
+// let score =33;
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" => 33
 //"33abc" => NaN
@@ -13,3 +13,17 @@ console.log(valueInNumber);
 //1 =>true; 0=>false
 //""=>false
 //"hitesh" =>true
+
+// ********************Operations *********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// basic maths opertions
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+console.log("2">1);
+//for comparison the datatype should be same
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//we cant use this type of comparision 
