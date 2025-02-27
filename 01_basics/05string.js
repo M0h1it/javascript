0ea0fbb611b6dda806f92535dc6347b6661954ad
Mohit Kumar <94331227+M0h1it@users.noreply.github.com>
2025-02-27 18:30:10 +0000

    string updated

--- 01_basics/05string.js
+++ 01_basics/05string.js
@@ -6,3 +6,35 @@ const repoCount = 50
 //string interpolation
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
+const gameName = new String('hiteshhhc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+
+console.log(anotherString);
+
+const newStringOne = "    hitesh   "
+console.log(newStringOne);
+console.log(newStringOne.trim());//remove spaces
+
+const url = "https://hitesh.com/hitesh%20chobey"
+console.log(url.replace('%20', '-'))
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
